85e76320d7d475dca6c7397251244e606b05602d
Misha Kirichenko <m.kirichenko.dev@gmail.com>
2023-06-25 00:06:19 +0400

    initial commit

--- src/crop/crop.controller.ts
+++ src/crop/crop.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { CropService } from './crop.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller('crop')
 export class CropController {
+  constructor(private cropService: CropService) {}
   @UseGuards(AuthGuard)
   @Post()
-  crop() {
-    return 'crop crop';
+  @UseInterceptors(FileInterceptor('video'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const allowedExtensions = ['.mp4', '.avi', '.mkv'];
+    const extension = extname(file.originalname).toLowerCase();
+
+    if (!allowedExtensions.includes(extension)) {
+      throw new BadRequestException(
+        `Only ${allowedExtensions.join(',')} files are allowed!`,
+      );
+    }
+    return this.cropService.cropVideo(file.buffer);
   }
 }
--- src/crop/crop.service.ts
+++ src/crop/crop.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import * as ffmpeg from 'fluent-ffmpeg';
+import { promises as fsPromises } from 'fs';
+import { join } from 'path';
 
 @Injectable()
-export class CropService {}
+export class CropService {
+  async cropVideo(buffer: Buffer): Promise<void> {
+    const uploadsDirectory = './uploads';
+    await fsPromises.mkdir(uploadsDirectory, { recursive: true }); // Create the uploads directory if it doesn't exist
+
+    const tempFilePath = join(uploadsDirectory, 'temp_video.mp4');
+    await fsPromises.writeFile(tempFilePath, buffer); // Write the buffer to a temporary file
+
+    const trimmedStartPath = join(uploadsDirectory, 'trimmed_start_video.mp4');
+    await this.trimVideo(tempFilePath, trimmedStartPath, 0, 300); // 300 seconds = 5 minutes
+
+    const duration = await this.getVideoDuration(tempFilePath);
+    const trimmedEndPath = join(uploadsDirectory, 'trimmed_end_video.mp4');
+    await this.trimVideo(tempFilePath, trimmedEndPath, duration - 300, duration);
+
+    // Delete the temporary file
+    await fsPromises.unlink(tempFilePath);
+
+    console.log('Video processed successfully');
+  }
+
+  async trimVideo(
+    inputFilePath: string,
+    outputFilePath: string,
+    start: number,
+    end: number,
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      ffmpeg(inputFilePath)
+        .setStartTime(start)
+        .setDuration(end - start)
+        .output(outputFilePath)
+        .on('end', () => {
+          resolve();
+        })
+        .on('error', (error) => {
+          reject(error);
+        })
+        .run();
+    });
+  }
+
+  private getVideoDuration(inputPath: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(inputPath, (err, metadata) => {
+        if (err) {
+          reject(err);
+        } else {
+          const duration = metadata.format.duration;
+          resolve(duration);
+        }
+      });
+    });
+  }
+}
